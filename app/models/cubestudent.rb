c176a7a4be200083325fd6bf0e00de4d16bf2375
dhwani bhatt <dhwanibhatt3007@gmail.com>
2014-04-23 16:51:55 +0530

    sneh

--- app/models/cubestudent.rb
+++ app/models/cubestudent.rb
@@ -2,8 +2,8 @@ class Cubestudent < ActiveRecord::Base
   has_merit
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  #  :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,:confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   attr_accessible :email, :password, :password_confirmation
   acts_as_tagger
