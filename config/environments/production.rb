0a5a6cee74305fe9970444c19c4560fbd972e2ab
dhwani bhatt <dhwanibhatt3007@gmail.com>
2014-04-23 14:41:42 +0530

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,8 +10,9 @@ Project::Application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
@@ -20,20 +21,21 @@ Project::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  
 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  #config.assets.version = '1.0'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -63,14 +65,29 @@ Project::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
+config.action_mailer.perform_deliveries = true
+
+config.active_support.deprecation = :silence
   config.i18n.fallbacks = true
+  config.action_mailer.default_url_options = { :host => 'http://fast-waters-2957.herokuapp.com/' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => "dhwanibhatt3007@gmail.com",
+    :password => "dhwani30071993"
+}
 
   # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
+  
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
